512e760ec483fb47f3450d6194d962de6b261eda
Johnathan Waugh <superjedi224@gmail.com>
2019-05-14 16:11:59 -0400

    Add files via upload

--- src/main/java/sj224/mods/nether_dungeon/NetherMod.java
+++ src/main/java/sj224/mods/nether_dungeon/NetherMod.java
@@ -14,7 +14,7 @@ public class NetherMod
 {
     public static final String MODID = "nether_dungeon";
     public static final String NAME = "Example Mod";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.0.1";
     
     public static int dungeonProbability;
     public static ResourceLocation lootTable;
@@ -24,8 +24,9 @@ public class NetherMod
     public void preInit(FMLPreInitializationEvent event)
     {
         Configuration cfg=new Configuration(event.getSuggestedConfigurationFile());
-        dungeonProbability=cfg.getInt("dungeon_probability","",275,1,1000,"Dungeon attempts per 1000 chunks");
-        lootTable=new ResourceLocation(cfg.getString("loot_table","", "nether_dungeon:chests/nether_dungeon","Loot table used by nether dungeon chests"));
+        cfg.load();
+        dungeonProbability=cfg.getInt("dungeon_probability","nether_dungeon",275,1,1000,"Dungeon attempts per 1000 chunks");
+        lootTable=new ResourceLocation(cfg.getString("loot_table","nether_dungeon", "nether_dungeon:chests/nether_dungeon","Loot table used by nether dungeon chests"));
         LootTableList.register(lootTable);
         cfg.save();
     }
